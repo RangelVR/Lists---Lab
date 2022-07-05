db1055826dc1fb5d3d607bc22c548be526460044
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-07-06 00:32:57 +0300

    Add files via upload

---
+++ 02. Gauss' Trick/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> nums = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            for (int i = 0; i < nums.Count - 1; i++)
+            {
+                nums[i] += nums[nums.Count - 1];
+                nums.RemoveAt(nums.Count - 1);
+            }
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
