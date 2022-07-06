6d63b8b56cc74da936363b1edea1a7db1b5812bc
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-07-06 23:23:09 +0300

    Add files via upload

---
+++ 05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> nums = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            nums.RemoveAll(x => x < 0); //nums.RemoveAll(x => x < 0);
+            nums.Reverse();
+
+            if (nums.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", nums));
+            }
+        }
+    }
+}
