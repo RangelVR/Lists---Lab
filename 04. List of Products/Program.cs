6386642592955accf74229520f4256837c9d8e9e
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-07-06 22:56:02 +0300

    Add files via upload

---
+++ 04. List of Products/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> prodName = new List<string>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine();
+                prodName.Add(product);
+            }
+            prodName.Sort();
+
+            for (int i = 0; i <= prodName.Count - 1; i++)
+            {
+                Console.WriteLine($"{i + 1}.{prodName[i]}");
+            }
+        }
+    }
+}
