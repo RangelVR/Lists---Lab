3e7139e9da8240feb57c7b2755f8057b21d53783
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-19 21:45:53 +0200

    Update Program.cs

--- 05. Remove Negatives and Reverse/Program.cs
+++ 05. Remove Negatives and Reverse/Program.cs
@@ -1,30 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _05._Remove_Negatives_and_Reverse
+namespace _05.Remove_Negatives_and_Reverse
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<double> nums = Console.ReadLine()
-                .Split()
-                .Select(double.Parse)
-                .ToList();
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).Reverse().ToList();
+            nums.RemoveAll(x => x < 0);
 
-            nums.RemoveAll(x => x < 0); 
-            nums.Reverse();
-
-            if (nums.Count == 0)
-            {
-                Console.WriteLine("empty");
-            }
-            else
+            if (nums.Sum() > 0)
             {
                 Console.WriteLine(string.Join(" ", nums));
             }
-               
+            else
+            {
+                Console.WriteLine("empty");
+            }
         }
     }
 }
