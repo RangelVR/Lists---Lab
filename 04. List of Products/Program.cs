ec9fbcc36af049b01ecfbe08d232937839000667
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-19 21:18:27 +0200

    Update Program.cs

--- 04. List of Products/Program.cs
+++ 04. List of Products/Program.cs
@@ -1,25 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace _04._List_of_Products
+namespace _04.List_of_Products
 {
     class Program
     {
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            List<string> prodName = new List<string>();
-            
+            List<string> productsList = new List<string>();
+
             for (int i = 0; i < n; i++)
             {
                 string product = Console.ReadLine();
-                prodName.Add(product);
+                productsList.Add(product);
             }
-            prodName.Sort();
-
-            for (int i = 0; i <= prodName.Count - 1; i++)
+            productsList.Sort();
+            for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine($"{i + 1}.{prodName[i]}");
+                Console.WriteLine($"{i}.{productsList[i - 1]}");
             }
         }
     }
