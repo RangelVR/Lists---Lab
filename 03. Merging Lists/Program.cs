88877a6c55dd148733dc262c053c043fda647fce
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-12 14:23:17 +0200

    Update Program.cs

--- 03. Merging Lists/Program.cs
+++ 03. Merging Lists/Program.cs
@@ -1,42 +1,54 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
 
-namespace _03.Merging_Lists
+List<int> result = new List<int>();
+
+int theSmallestOne = Math.Min(firstList.Count, secondList.Count);
+
+for (int i = 0; i < theSmallestOne; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<int> list1 = Console.ReadLine().Split().Select(int.Parse).ToList();
-            List<int> list2 = Console.ReadLine().Split().Select(int.Parse).ToList();
-            List<int> mergedList = new List<int>();
-
-            if (list1.Count > list2.Count)
-            {
-                for (int i = 0; i < list2.Count; i++)
-                {
-                    mergedList.Add(list1[i]);
-                    mergedList.Add(list2[i]);
-                }
-                for (int i = list2.Count; i < list1.Count; i++)
-                {
-                    mergedList.Add(list1[i]);
-                }
-            }
-            else
-            {
-                for (int i = 0; i < list1.Count; i++)
-                {
-                    mergedList.Add(list1[i]);
-                    mergedList.Add(list2[i]);
-                }
-                for (int i = list1.Count; i < list2.Count; i++)
-                {
-                    mergedList.Add(list2[i]);
-                }
-            }
-            Console.WriteLine(string.Join(" ", mergedList));
-        }
-    }
+    result.Add(firstList[i]); 
+    result.Add(secondList[i]); 
 }
+
+if (firstList.Count > secondList.Count)
+{
+    result.AddRange(firstList.Skip(secondList.Count));
+}
+else if(secondList.Count > firstList.Count)
+{
+    result.AddRange(secondList.Skip(firstList.Count));
+}
+
+Console.WriteLine(string.Join(" ", result));
+
+-------------------------------------------------------------------------------------
+//List<int> list1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+//List<int> list2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+//List<int> mergedList = new List<int>();
+
+//if (list1.Count > list2.Count)
+//{
+//    for (int i = 0; i < list2.Count; i++)
+//    {
+//        mergedList.Add(list1[i]);
+//        mergedList.Add(list2[i]);
+//    }
+//    for (int i = list2.Count; i < list1.Count; i++)
+//    {
+//        mergedList.Add(list1[i]);
+//    }
+//}
+//else
+//{
+//    for (int i = 0; i < list1.Count; i++)
+//    {
+//        mergedList.Add(list1[i]);
+//        mergedList.Add(list2[i]);
+//    }
+//    for (int i = list1.Count; i < list2.Count; i++)
+//    {
+//        mergedList.Add(list2[i]);
+//    }
+//}
+//Console.WriteLine(string.Join(" ", mergedList));
