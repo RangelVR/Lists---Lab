1c961c51fb533b56da13f754c141b795126c201f
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-12 14:30:40 +0200

    Update Program.cs

--- 07. List Manipulation Advanced/Program.cs
+++ 07. List Manipulation Advanced/Program.cs
@@ -1,95 +1,103 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
 
-namespace _06.List_Manipulation_Advansed
+bool isMadeChanges = false;
+string command;
+
+while ((command = Console.ReadLine()) != "end")
 {
-    class Program
+    string[] currCommArr = command.Split().ToArray();
+
+    switch (currCommArr[0])
     {
-        static void Main(string[] args)
-        {
-            List<string> nums = Console.ReadLine().Split().ToList();
-            string[] command = Console.ReadLine().Split().ToArray();
-            bool chanches = false;
+        case "Add":
+            int numberToAdd = int.Parse(currCommArr[1]);
+            list.Add(numberToAdd);
+            isMadeChanges = true;
+            break;
 
-            while (command[0] != "end")
-            {
-                if (command.Contains("Add"))
-                {
-                    nums.Add(command[1]);
-                    chanches = true;
-                }
-                else if (command.Contains("Remove"))
-                {
-                    nums.Remove(command[1]);
-                    chanches = true;
-                }
-                else if (command.Contains("RemoveAt"))
-                {
-                    nums.RemoveAt(int.Parse(command[1]));
-                    chanches = true;
-                }
-                else if (command.Contains("Insert"))
-                {
-                    nums.Insert(int.Parse(command[2]), command[1]);
-                    chanches = true;
-                }
-                else if (command.Contains("Contains"))
-                {
-                    if (nums.Contains(command[1]))
-                    {
-                        Console.WriteLine("Yes");
-                    }
-                    else
-                    {
-                        Console.WriteLine("No such number");
-                    }
-                }
-                else if (command.Contains("PrintEven"))
-                {
-                    List<int> evenNums = nums.Select(int.Parse).ToList();
-                    evenNums.RemoveAll(x => x % 2 != 0);
-                    Console.WriteLine(string.Join(" ", evenNums));
-                }
-                else if (command.Contains("PrintOdd"))
-                {
-                    List<int> oddNums = nums.Select(int.Parse).ToList();
-                    oddNums.RemoveAll(x => x % 2 == 0);
-                    Console.WriteLine(string.Join(" ", oddNums));
-                }
-                else if (command.Contains("GetSum"))
-                {
-                    List<int> sumNums = nums.Select(int.Parse).ToList();
-                    Console.WriteLine(sumNums.Sum());
-                }
-                else if (command.Contains("Filter"))
-                {
-                    int number = int.Parse(command[2]);
-                    List<int> numbers = nums.Select(int.Parse).ToList();
-                    switch (command[1])
-                    {
-                        case ">":
-                            Console.WriteLine(string.Join(" ", numbers.FindAll(x => x > number)));
-                            break;
-                        case "<":
-                            Console.WriteLine(string.Join(" ", numbers.FindAll(x => x < number)));
-                            break;
-                                case ">=":
-                            Console.WriteLine(string.Join(" ", numbers.FindAll(x => x >= number)));
-                            break;
-                        case "<=":
-                            Console.WriteLine(string.Join(" ", numbers.FindAll(x => x <= number)));
-                            break;
-                    }
-                }
-                command = Console.ReadLine().Split().ToArray();
-            }
+        case "Remove":
+            int numberToRemove = int.Parse(currCommArr[1]);
+            list.Remove(numberToRemove);
+            isMadeChanges = true;
+            break;
 
-            if (chanches)
-            {
-                Console.WriteLine(string.Join(" ", nums));
-            }
-            
-        }
+        case "RemoveAt":
+            int idexToRemove = int.Parse(currCommArr[1]);
+            list.RemoveAt(idexToRemove);
+            isMadeChanges = true;
+            break;
+
+        case "Insert":
+            int number = int.Parse(currCommArr[1]);
+            int index = int.Parse(currCommArr[2]);
+            list.Insert(index, number);
+            isMadeChanges = true;
+            break;
+
+        case "Contains":
+            IsTheNumberExist(list, currCommArr);
+            break;
+
+        case "PrintEven":
+            Console.WriteLine(string.Join(" ", list.Where(x => x % 2 == 0)));
+            break;
+
+        case "PrintOdd":
+            Console.WriteLine(string.Join(" ", list.Where(x => x % 2 != 0)));
+            break;
+
+        case "GetSum":
+            Console.WriteLine(list.Sum());
+            break;
+
+        case "Filter":
+            PrintAllFilteredNumbers(list, currCommArr);
+            break;
+    }
+}
+
+if (isMadeChanges)
+{
+    Console.WriteLine(string.Join(" ", list));
+}
+
+
+
+static void IsTheNumberExist(List<int> list, string[] currCommArr) 
+{
+    int number = int.Parse(currCommArr[1]);
+
+    if (list.Contains(number))
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No such number");
+    }
+}
+
+static void PrintAllFilteredNumbers(List<int> list, string[] currCommArr)
+{
+    string condition = currCommArr[1];
+    int number = int.Parse(currCommArr[2]);
+
+    switch (condition)
+    {
+        case ">":
+            Console.WriteLine(string.Join(" ", list.Where(x => x > number)));
+            break;
+
+        case "<":
+            Console.WriteLine(string.Join(" ", list.Where(x => x < number)));
+            break;
+
+        case ">=":
+            Console.WriteLine(string.Join(" ", list.Where(x => x >= number)));
+            break;
+
+        case "<=":
+            Console.WriteLine(string.Join(" ", list.Where(x => x <= number)));
+            break;
     }
 }
