1db900c21fa1c9ff30be5fa51a591611ecb01e21
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-12 14:29:28 +0200

    Update Program.cs

--- 06. List Manipulation Basics/Program.cs
+++ 06. List Manipulation Basics/Program.cs
@@ -1,37 +1,61 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
 
-namespace _06.List_Manipulation_Basics
+string command;
+
+while ((command = Console.ReadLine()) != "end")
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<string> nums = Console.ReadLine().Split().ToList();
-            string[] command = Console.ReadLine().Split().ToArray();
+    string[] currCommArr = command.Split().ToArray();
 
-            while (command[0] != "end")
-            {
-                if (command.Contains("Add"))
-                {
-                    nums.Add(command[1]);
-                }
-                else if (command.Contains("Remove"))
-                {
-                    nums.Remove(command[1]);
-                }
-                else if (command.Contains("RemoveAt"))
-                {
-                    nums.RemoveAt(int.Parse(command[1]));
-                }
-                else if (command.Contains("Insert"))
-                {
-                    nums.Insert(int.Parse(command[2]), command[1]);
-                }
-                command = Console.ReadLine().Split().ToArray();
-            }
-            Console.WriteLine(string.Join(" ", nums));
-        }
+	switch (currCommArr[0])
+	{
+		case "Add":
+            int numberToAdd = int.Parse(currCommArr[1]);
+            list.Add(numberToAdd); 
+            break;
+
+        case "Remove":
+            int numberToRemove = int.Parse(currCommArr[1]);
+            list.Remove(numberToRemove);
+            break;
+
+        case "RemoveAt":
+            int idexToRemove = int.Parse(currCommArr[1]);
+            list.RemoveAt(idexToRemove);
+            break;
+
+        case "Insert":
+            int number = int.Parse(currCommArr[1]);
+            int index = int.Parse(currCommArr[2]);
+            list.Insert(index, number);
+            break;
     }
 }
+
+Console.WriteLine(string.Join(" ", list));
+
+-----------------------------------------------------------------------
+//List<string> nums = Console.ReadLine().Split().ToList();
+//string[] command = Console.ReadLine().Split().ToArray();
+
+//while (command[0] != "end")
+//{
+//    if (command.Contains("Add"))
+//    {
+//        nums.Add(command[1]);
+//    }
+//    else if (command.Contains("Remove"))
+//    {
+//        nums.Remove(command[1]);
+//    }
+//    else if (command.Contains("RemoveAt"))
+//    {
+//        nums.RemoveAt(int.Parse(command[1]));
+//    }
+//    else if (command.Contains("Insert"))
+//    {
+//        nums.Insert(int.Parse(command[2]), command[1]);
+//    }
+//    command = Console.ReadLine().Split().ToArray();
+//}
+
+//Console.WriteLine(string.Join(" ", nums));
